cd774e622e9317f137e8b6300652d68a2dccb15a
André Victor Stephanes Valvassori <an.victor@hotmail.com>
2019-05-11 10:27:24 -0300

    Add files via upload

---
+++ CineReviewX/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CineReviewX
+//
+//  Created by PUCPR on 11/05/19.
+//  Copyright © 2019 FcZDevelopment. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
